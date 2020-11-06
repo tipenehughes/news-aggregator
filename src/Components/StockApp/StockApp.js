4a19fbf85117eb5610087590a1f1a0c0df3b8efa
Tipene Hughes <tipenehughes@gmail.com>
2020-11-06 15:05:15 -0500

    Added dynamic display based on API Data for StockApp and refactored in to seperate components

--- src/Components/StockApp/StockApp.js
+++ src/Components/StockApp/StockApp.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
 import Spinner from "../Spinner";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import TickerBoxes from "./TickerBoxes";
 
 import styles from "../../Css/StockApp.module.css";
 
 const StockApp = () => {
     const APP_KEY = process.env.REACT_APP_IEX_KEY;
 
-    const [price, setPrice] = useState({});
+    const [price, setPrice] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
@@ -22,7 +21,6 @@ const StockApp = () => {
         const data = await response.json();
         setPrice(data);
         setIsLoading(false);
-        console.log(price);
     };
 
     return isLoading ? (
@@ -33,142 +31,7 @@ const StockApp = () => {
                 <h2>Stocks</h2>
             </div>
             <div className={styles.stockTilesContainer}>
-                <div className={styles.stockTiles}>
-                    <div className={styles.tickerSymbol}>
-                        <h4>SPY</h4>
-                    </div>
-                    <div className={styles.stockName}>
-                        <p>SPDR S&P 500 ETF Trust</p>
-                    </div>
-                    <div className={styles.currentPrice}>
-                        <p>$326.43</p>
-                    </div>
-                    <div className={styles.movement}>
-                        <span>
-                            <FontAwesomeIcon icon={faArrowUp} />
-                        </span>
-                        <p>2.13%</p>
-                    </div>
-                </div>
-                <div className={styles.stockTiles}>
-                    <div className={styles.tickerSymbol}>
-                        <h4>SPY</h4>
-                    </div>
-                    <div className={styles.stockName}>
-                        <p>SPDR S&P 500 ETF Trust</p>
-                    </div>
-                    <div className={styles.currentPrice}>
-                        <p>$326.43</p>
-                    </div>
-                    <div className={styles.movement}>
-                        <span>
-                            <FontAwesomeIcon icon={faArrowUp} />
-                        </span>
-                        <p>2.13%</p>
-                    </div>
-                </div>
-                <div className={styles.stockTiles}>
-                    <div className={styles.tickerSymbol}>
-                        <h4>SPY</h4>
-                    </div>
-                    <div className={styles.stockName}>
-                        <p>SPDR S&P 500 ETF</p>
-                    </div>
-                    <div className={styles.currentPrice}>
-                        <p>$326.43</p>
-                    </div>
-                    <div className={styles.movement}>
-                        <span>
-                            <FontAwesomeIcon icon={faArrowUp} />
-                        </span>
-                        <p>2.13%</p>
-                    </div>
-                </div>
-                <div className={styles.stockTiles}>
-                    <div className={styles.tickerSymbol}>
-                        <h4>SPY</h4>
-                    </div>
-                    <div className={styles.stockName}>
-                        <p>SPDR S&P 500 ETF Trust</p>
-                    </div>
-                    <div className={styles.currentPrice}>
-                        <p>$326.43</p>
-                    </div>
-                    <div className={styles.movement}>
-                        <span>
-                            <FontAwesomeIcon icon={faArrowUp} />
-                        </span>
-                        <p>2.13%</p>
-                    </div>
-                </div>
-                <div className={styles.stockTiles}>
-                    <div className={styles.tickerSymbol}>
-                        <h4>SPY</h4>
-                    </div>
-                    <div className={styles.stockName}>
-                        <p>SPDR S&P 500 ETF Trust</p>
-                    </div>
-                    <div className={styles.currentPrice}>
-                        <p>$326.43</p>
-                    </div>
-                    <div className={styles.movement}>
-                        <span>
-                            <FontAwesomeIcon icon={faArrowUp} />
-                        </span>
-                        <p>2.13%</p>
-                    </div>
-                </div>
-                <div className={styles.stockTiles}>
-                    <div className={styles.tickerSymbol}>
-                        <h4>SPY</h4>
-                    </div>
-                    <div className={styles.stockName}>
-                        <p>SPDR S&P 500 ETF Trust</p>
-                    </div>
-                    <div className={styles.currentPrice}>
-                        <p>$326.43</p>
-                    </div>
-                    <div className={styles.movement}>
-                        <span>
-                            <FontAwesomeIcon icon={faArrowUp} />
-                        </span>
-                        <p>2.13%</p>
-                    </div>
-                </div>
-                <div className={styles.stockTiles}>
-                    <div className={styles.tickerSymbol}>
-                        <h4>SPY</h4>
-                    </div>
-                    <div className={styles.stockName}>
-                        <p>SPDR S&P 500 ETF Trust</p>
-                    </div>
-                    <div className={styles.currentPrice}>
-                        <p>$326.43</p>
-                    </div>
-                    <div className={styles.movement}>
-                        <span>
-                            <FontAwesomeIcon icon={faArrowUp} />
-                        </span>
-                        <p>2.13%</p>
-                    </div>
-                </div>
-                <div className={styles.stockTiles}>
-                    <div className={styles.tickerSymbol}>
-                        <h4>SPY</h4>
-                    </div>
-                    <div className={styles.stockName}>
-                        <p>SPDR S&P 500 ETF Trust</p>
-                    </div>
-                    <div className={styles.currentPrice}>
-                        <p>$326.43</p>
-                    </div>
-                    <div className={styles.movement}>
-                        <span>
-                            <FontAwesomeIcon icon={faArrowUp} />
-                        </span>
-                        <p>2.13%</p>
-                    </div>
-                </div>
+                <TickerBoxes price={price} />
             </div>
         </div>
     );
